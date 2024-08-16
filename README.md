82b49a1b611b07bd2548d5089d6aad5bf645c49e
to-mang2 <glglwlgud@naver.com>
2024-08-16 14:15:15 +0900

    svelte, vite로 바꾼 후  firebase deploy로 배포

---
+++ README.md
@@ -0,0 +1,1 @@
+# Welcome to Carroot-clone!!!
---
+++ src/pages/Main.svelte
@@ -0,0 +1,61 @@
+<script>
+  let hour = new Date().getHours();
+  let min = new Date().getMinutes();
+</script>
+
+<header>
+  <div class="info-bar">
+    <div class="info-bar__time">{hour}:{min}</div>
+    <div class="info-bar__icons">
+      <img src="assets/chart-bar.svg" />
+      <img src="assets/wifi.svg" />
+      <img src="assets/battery.svg" />
+    </div>
+  </div>
+  <div class="menu-bar">
+    <div class="menu-bar__location">
+      <div>역삼 1동</div>
+      <div class="menu-bar__location-icon">
+        <img src="assets/arrow.svg" />
+      </div>
+    </div>
+    <div class="menu-bar__icons">
+      <img src="assets/search.svg" />
+      <img src="assets/menu.svg" />
+      <img src="assets/alert.svg" />
+    </div>
+  </div>
+</header>
+
+<main>
+  <a class="write-btn" href="#/write">+ 글쓰기</a>
+</main>
+
+<footer>
+  <div class="footer-block">
+    <div class="footer-icons">
+      <div class="footer-icons__img"><img src="assets/home.svg" /></div>
+      <div class="footer-icons__desc">홈</div>
+    </div>
+    <div class="footer-icons">
+      <div class="footer-icons__img"><img src="assets/doc.svg" /></div>
+      <div class="footer-icons__desc">동네생활</div>
+    </div>
+    <div class="footer-icons">
+      <div class="footer-icons__img"><img src="assets/loaction.svg" /></div>
+      <div class="footer-icons__desc">내 근처</div>
+    </div>
+    <div class="footer-icons">
+      <div class="footer-icons__img"><img src="assets/chat.svg" /></div>
+      <div class="footer-icons__desc">채팅</div>
+    </div>
+    <div class="footer-icons">
+      <div class="footer-icons__img"><img src="assets/user.svg" /></div>
+      <div class="footer-icons__desc">나의 당근</div>
+    </div>
+  </div>
+</footer>
+<div class="media-info-msg">화면 사이즈를 줄여주세요.</div>
+
+<style>
+</style>
