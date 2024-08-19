c5cbdc5d772fff1d85b40910dc7657f21b0a0fe2
to-mang2 <glglwlgud@naver.com>
2024-08-20 00:27:40 +0900

    .env 활용

--- firebase.js
+++ firebase.js
@@ -1,23 +1,18 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
-import { getStorage } from "firebase/storage";
-import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD0RyUNcmsWgHhsNlvaN3CwQHmTBaYMktQ",
-  authDomain: "carrot-market-85f77.firebaseapp.com",
-  databaseURL:
-    "https://carrot-market-85f77-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "carrot-market-85f77",
-  storageBucket: "carrot-market-85f77.appspot.com",
-  messagingSenderId: "291271889262",
-  appId: "1:291271889262:web:a0c1e56d01be61a48ab17f",
+  apiKey: import.meta.env.VITE_APP_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DB_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MSG_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Realtime Database and get a reference to the service
-const database = getDatabase(app);
-const storage = getStorage(app);
-const auth = getAuth(app);
